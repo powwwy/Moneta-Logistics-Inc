cdd2438d97dd929be25fffea48cd9fc7b7ce717d
Sajjad Harunany <sajjadharunany@gmail.com>
2025-06-25 22:00:41 +0300

    Finalised CountryManager.java according to requirements

--- System/handler/DBUtil.java
+++ System/handler/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/moneta_logistics";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Shaan@004";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
