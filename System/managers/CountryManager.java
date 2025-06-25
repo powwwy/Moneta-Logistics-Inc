d25bf16e26183e37103bd627b89227f830510bb7
Sajjad Harunany <sajjadharunany@gmail.com>
2025-06-25 14:57:24 +0300

    Added Trade Option

--- System/managers/CountryManager.java
+++ System/managers/CountryManager.java
@@ -11,24 +11,25 @@ public class CountryManager extends User{
     public void showMenu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\n=== Logged in as " + Country.currentCountryName + " ===");
-        System.out.println("1. View Ships");
-        System.out.println("2. View Ports");
-        System.out.println("3. View Account Balance");
-        System.out.println("4. Update Ship");
-        System.out.println("5. Update Port");
-        System.out.println("6. Update Balance");
-        System.out.println("7. Logout");
+        System.out.println("1. Initiate Trade");
+        System.out.println("2. View Ships");
+        System.out.println("3. View Ports");
+        System.out.println("4. View Account Balance");
+        System.out.println("5. Update Ship");
+        System.out.println("6. Update Port");
+        System.out.println("7. Update Balance");
+        System.out.println("8. Logout");
         System.out.print("Choose an option: ");
         String option = scanner.nextLine();
 
         switch (option) {
-            case "1" -> Country.viewShips();
-            case "2" -> Country.viewPorts();
-            case "3" -> Country.viewAccountBalance();
-            case "4" -> Country.updateShip();
-            case "5" -> Country.updatePort();
-            case "6" -> Country.updateBalance();
-            case "7" -> Country.logout();
+            case "2" -> Country.viewShips();
+            case "3" -> Country.viewPorts();
+            case "4" -> Country.viewAccountBalance();
+            case "5" -> Country.updateShip();
+            case "6" -> Country.updatePort();
+            case "7" -> Country.updateBalance();
+            case "8" -> Country.logout();
             default -> System.out.println("Invalid option, try again.");
         }
     }
